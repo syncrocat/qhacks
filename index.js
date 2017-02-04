c60b9614a5726f41a47ea18a1eb749a963ae31d8
LucasBullen <bullenlucas@gmail.com>
2017-02-04 10:15:25 -0500

    update user list endpoint

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//setting up the server
+
 var fs = require('fs');
 var https = require('https');
 var http = require('http');
@@ -9,7 +11,6 @@ var app = express();
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-//app.use(express.json());       // to support JSON-encoded bodies
 
 var credentials = {key: key, cert: cert};
 var httpServer = http.createServer(app);
@@ -22,18 +23,6 @@ app.get('/', function(req,res) {
     res.send('hello');
 });
 
-/*
-var SpotifyWebApi = require('spotify-web-api-node');
-var config = require('./config');
-
-var spotifyApi = new SpotifyWebApi({
-  clientId: "294422f175f2404ca3be4840769aea24",
-  clientSecret: config.clientSecret,
-  redirectUri: "http://lucasbullen.com",
-});
-
-spotifyApi.setAccessToken("BQDpJ1yH71Ytvf0T9h8sZCjhyyOCyUu6KkZJvZ51iWXfe64KyBy_T6PluQUT9aoOhnQDWqX3oAg9biXMXqy6_sZh6z0DRoO6vfQdvNW3m2wZsQPtO_4BuxBa1KzqBxOY4gDMgT5F5OrokhbaLgymZSRx2_4GhhLNtQsxTyyDypSpEq0iFkNg063Ttaozk9T2awXl6k7QRV31FLLP5UG_Y4TsDNstYnU5rp8mMdJDyRnVtpmetdF_CX6FwxqVQdF_KB07m9O0U_NrtTtvXVPTQ1mZupYbgKHXzZANQy05DcHS73CzNJw");
-*/
 //mongoDB
 
 var mongodb = require('mongodb');
@@ -48,16 +37,17 @@ client.connect('mongodb://localhost:27017/qhacks', function (err, db) {
     }
 });
 
+//adds or updates a router object
 app.post("/routers/:id",function(request, response){
     var id = request.params.id;
 
-	if(request.body.access_token == null ||
+	if(id == null || request.body.access_token == null ||
 		request.body.display_name == null){
 		console.log("bad router");
 		var obj = { error : "missing parameter"};
 
-	    response.writeHead(400, {"Content-Type": "application/json"});
-	    response.write(JSON.stringify(obj));
+	    response.setHeader(400, {"Content-Type": "application/json"});
+	    response.send(JSON.stringify(obj));
 	    return;
 	}
 
@@ -72,41 +62,51 @@ app.post("/routers/:id",function(request, response){
     collection.insertOne(router)
     .then(function(result) {
         console.log("Saved");
-        response.writeHead(200, {"Content-Type": "application/json"});
-	    response.write(JSON.stringify({success:true}));
+        response.setHeader(200, {"Content-Type": "application/json"});
+	    response.send(JSON.stringify({success:true}));
     });
 });
 
+//updates the list of users on a router
 app.post("/routers/:id/users",function(request, response){
     var id = request.params.id;
+    var macAddresses = request.body.macAddresses;
 
-   	if(request.body.macAddresses == null){
+   	if(id == null || request.body.macAddresses == null){
 		console.log("bad list");
 		var obj = { error : "missing parameter"};
 
-	    response.writeHead(400, {"Content-Type": "application/json"});
-	    response.write(JSON.stringify(obj));
+	    response.setHeader(400, {"Content-Type": "application/json"});
+	    response.send(JSON.stringify(obj));
+	    return;
 	}
 
-	/*
-		TODO:
-			get last list
-			compare to new list given list
-			remove all current rel with router
-			add new
-			tell spotify
-	*/
+	var user_router_rel_collection = database.collection('user_router_rel');
+	var users_collection = database.collection('users');
+
+	user_router_rel_collection.remove({'router_id':id});
+	for (var i = 0; i < macAddresses.length; i++) {
+		users_collection.findOne({'mac_address':macAddresses[i]}).then(function(result) {
+	        var new_rel = {
+				router_id: id,
+				spotify_id: result.spotify_id
+			};
+			user_router_rel_collection.insertOne(new_rel);
+	    });
+	}
 });
 
+//adds or updates a user
 app.post("/users/:id",function(request,response){
 	var id = request.params.id;
-	if(request.body.macAddress == null ||
+	if(id == null || request.body.macAddress == null ||
 		request.body.display_name == null){
 		console.log("bad user");
 		var obj = { error : "missing parameter"};
 
-	    response.writeHead(400, {"Content-Type": "application/json"});
-	    response.write(JSON.stringify(obj));
+	    response.setHeader(400, {"Content-Type": "application/json"});
+	    response.send(JSON.stringify(obj));
+	    return;
 	}
 
 	var user = {
@@ -120,8 +120,47 @@ app.post("/users/:id",function(request,response){
     collection.insertOne(user)
     .then(function(result) {
         console.log("Saved");
-        response.writeHead(200, {"Content-Type": "application/json"});
-	    response.write(JSON.stringify({success:true}));
+        response.setHeader(200, {"Content-Type": "application/json"});
+	    response.send(JSON.stringify({success:true}));
     });
 });
 
+//return list of current users
+app.get("/routers/:id/users",function(request,response){
+	var id = request.params.id;
+	if(id == null){
+		var obj = { error : "missing id"};
+
+	    response.setHeader(400, {"Content-Type": "application/json"});
+	    response.send(JSON.stringify(obj));
+	    return;
+	}
+	var user_router_rel_collection = database.collection('user_router_rel');
+	user_router_rel_collection.find({'router_id':id}).then(function(result) {
+		for (var i = 0; i < result.length; i++) {
+			var users_collection = database.collection('users');
+			var users = [];
+
+			users_collection.findOne({'spotify_id':result[i].spotify_id}).then(function(result) {
+				users.push(result);
+			});
+		}
+        response.setHeader(200, {"Content-Type": "application/json"});
+	    response.send(JSON.stringify(users));
+    });
+});
+
+/***********************************
+		Spotify
+***********************************/
+
+var SpotifyWebApi = require('spotify-web-api-node');
+var config = require('./config');
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: "294422f175f2404ca3be4840769aea24",
+  clientSecret: config.clientSecret,
+  redirectUri: "http://lucasbullen.com",
+});
+
+spotifyApi.setAccessToken("BQDpJ1yH71Ytvf0T9h8sZCjhyyOCyUu6KkZJvZ51iWXfe64KyBy_T6PluQUT9aoOhnQDWqX3oAg9biXMXqy6_sZh6z0DRoO6vfQdvNW3m2wZsQPtO_4BuxBa1KzqBxOY4gDMgT5F5OrokhbaLgymZSRx2_4GhhLNtQsxTyyDypSpEq0iFkNg063Ttaozk9T2awXl6k7QRV31FLLP5UG_Y4TsDNstYnU5rp8mMdJDyRnVtpmetdF_CX6FwxqVQdF_KB07m9O0U_NrtTtvXVPTQ1mZupYbgKHXzZANQy05DcHS73CzNJw");
