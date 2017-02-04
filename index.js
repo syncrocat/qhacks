4d96af70134a552b7ab3b97b6ce3dc9151d1ae8c
LucasBullen <bullenlucas@gmail.com>
2017-02-04 12:02:23 -0500

    all ma stuff

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var cert = fs.readFileSync('cert.pem');
 var express = require('express');
 var app = express();
 
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser').json();
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(function(req, res, next) {
   res.setHeader("Content-Type", "application/json")
