44b7eb017b818ca87fe7c05dabef6e0ac2b96bf0
LucasBullen <bullenlucas@gmail.com>
2017-02-05 01:32:04 -0500

    missed s

--- spotify/app.js
+++ spotify/app.js
@@ -130,7 +130,7 @@ exports.load = function(
     // the tunable track attributes we care about
 
     var authOptions = {
-      url: 'https://api.spotify.com/v1/recommendations?seed_artist=' + artists,
+      url: 'https://api.spotify.com/v1/recommendations?seed_artists=' + artists,
       headers: {
         'Authorization': 'Bearer ' + accessToken
       },
